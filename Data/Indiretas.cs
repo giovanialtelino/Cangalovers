c9bf942c42f67e366608f0caa93bd1cecf1c54ac
giovanialtelino <giovanialtelino@hotmail.com>
2023-11-13 22:46:59 -0300

    template default

---
+++ Data/Indiretas.cs
@@ -0,0 +1,9 @@
+﻿namespace CangaLovers.Data
+{
+    public class Indiretas
+    {
+        public string Name { get; set; }
+        public string Indireta { get; set; }
+        public DateTime Horario { get; set; } = DateTime.Now;
+    }
+}
