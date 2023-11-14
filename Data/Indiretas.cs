ac8514b81be58c367a3ce9a66061df739fe77eee
giovanialtelino <giovanialtelino@hotmail.com>
2023-11-13 23:39:12 -0300

    adicionando inmemory database

--- Data/Indiretas.cs
+++ Data/Indiretas.cs
@@ -1,7 +1,13 @@
-﻿namespace CangaLovers.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CangaLovers.Data
 {
     public class Indiretas
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Indireta { get; set; }
         public DateTime Horario { get; set; } = DateTime.Now;
---
+++ Data/IndiretasContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace CangaLovers.Data
+{
+    public class IndiretasContext : DbContext
+    {
+        public IndiretasContext(DbContextOptions<IndiretasContext> options)
+          : base(options)
+        { 
+        }
+
+        public DbSet<Indiretas> Indiretas { get; set; }
+    }
+}
