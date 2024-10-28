44b9e0e6b6fee1d35861700494934859f5e5172d
Giovani Altelino <50564867+giovanialtelino@users.noreply.github.com>
2024-10-27 22:12:32 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 //{
 //    app.UseExceptionHandler("/Error");
 //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
+    app.UseHsts();
 //}
 
 //app.UseHttpsRedirection();
